6baf10a0f8576eab90e8eb5bf450ed3858132cb3
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2017-09-23 19:55:38 +0530

    Color mode styles implemented.

--- .postcss.config.js
+++ .postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-nested-props'),
     require('postcss-nested'),
+    require('postcss-color-function'),
     require('postcss-mixins'),
     require('postcss-font-magician'),
     require('postcss-cssnext'),
