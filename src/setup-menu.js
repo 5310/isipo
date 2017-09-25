45e9b8c5582afaac401bf580d8f8136ed176bd49
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2017-09-25 18:26:26 +0530

    Refactors elements.

--- src/setup-menu.js
+++ src/setup-menu.js
@@ -2,7 +2,8 @@ export default () => {
   const $html = document.querySelector('html')
 
   const $more = document.querySelector('aside .more')
-  const $menu = document.querySelector('aside .menu')
+  const $aside = document.querySelector('aside')
+  const $menu = $aside.querySelector('.menu')
 
   $menu.querySelector('.overlay').addEventListener('pointerdown', () => {
     $menu.classList.add('hide')
@@ -11,7 +12,7 @@ export default () => {
     $menu.classList.remove('hide')
   })
 
-  const $color = document.querySelector('aside .color')
+  const $color = $menu.querySelector('.color')
   const $colors = Array.from($color.children)
   let color = 0
   const changeColor = () => {
